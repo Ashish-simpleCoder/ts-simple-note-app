c6789b00a8afd9b9b33bd7654486a929506f2d72
Ashish-simpleCoder <ashishprajapati357753357@gmail.com>
2022-05-21 11:36:33 +0530

    [removed] useTheme hook import in App component [added] useEffect in the useTheme for toggling body theme [removed] memoization of the App component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,18 +1,9 @@
 import "./Styles/style.css"
-import { memo, useEffect } from "react"
 import Header from "./Components/Journal Components/Header/Header"
 import withRouter from "./Utility/Utility Components/withRouter"
 import withStore from "./Utility/Utility Components/withStore"
-import useTheme from "./Redux/hooks/useTheme"
 
 const App = ({children}: {children?: JSX.Element | JSX.Element[]}) => {
-
-    const {theme} = useTheme()
-    useEffect(() => {
-        document.body.classList.toggle('dark', theme)
-        localStorage.setItem('dark', JSON.stringify(theme))
-    }, [theme] )
-
 
     return(
         <>
@@ -21,4 +12,4 @@ const App = ({children}: {children?: JSX.Element | JSX.Element[]}) => {
         </>
     )
 }
-export default memo(() => withStore(() => withRouter(App)))
+export default () => withStore(() => withRouter(App))
--- client/src/Redux/hooks/useTheme.tsx
+++ client/src/Redux/hooks/useTheme.tsx
@@ -1,8 +1,15 @@
+import { useEffect } from "react"
 import { useDispatch, useSelector } from "react-redux"
 
 const useTheme = () => {
     const theme = useSelector((state: Theme) => state.theme.value)
     const dispatch = useDispatch()
+
+    useEffect(() => {
+        document.body.classList.toggle('dark', theme)
+        localStorage.setItem('dark', JSON.stringify(theme))
+    }, [theme] )
+
     return {theme, dispatch}
 }
 export default useTheme
